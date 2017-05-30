38fd35e5ca0cfb0fa8986dc560bb3592f2a1056b
3PAUS44\Roger Ng <roger.ng@3plearning.com>
2017-05-30 12:25:37 +1000

    data.json added

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var argv = require('optimist').argv;
 var filendir = require('filendir');
 var pathType = require('path-type');
 var controllerJs = require('./templates/controller-js');
+var dataJson = require('./templates/data-json');
 var indexHtml = require('./templates/index-html');
 var indexJs = require('./templates/index-js');
 var styleScss = require('./templates/style-scss');
@@ -22,8 +23,10 @@ pathType.dir(argv.g).then(function () {
 
   filendir.ws(dirPathName + '/images/img', styleScss());
   filendir.ws(dirPathName + '/controller.js', controllerJs());
+  filendir.ws(dirPathName + '/data.json', dataJson());
   filendir.ws(dirPathName + '/index.html', indexHtml());
   filendir.ws(dirPathName + '/index.js', indexJs());
   filendir.ws(dirPathName + '/style.scss', styleScss());
+
   console.log('Files generated');
 });
--- templates/data-json.js
+++ templates/data-json.js
@@ -1,5 +1,40 @@
 require('babel-register')();
 
 module.exports = function(compTitle, fileName) {
-  return ``;
+  return `{
+  "levels": {
+    "level": {
+      "categories": {
+        "category": {
+          "games": {
+            "game": {
+              "home": {
+                "header": {
+                  "text": ""
+                },
+                "footer": {
+                  "text": ""
+                },
+                "question": {}
+              },
+              "menu": {
+                "left": null,
+                "right": true,
+                "bottom": {}
+              },
+              "help": {
+                "gameName": "<game-name>",
+                "content ": "<h1>How to play?</h1><div>Add sample game instruction here....</div>"
+              },
+              "preload": {
+                "images": {},
+                "ahead": {}
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}`;
 }
